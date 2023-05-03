507f679c2e57a8054ccf7e499feb0ae1f673bb5f
algoldbe <45243333+algoldbe@users.noreply.github.com>
2023-05-02 18:17:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
