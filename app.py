c9059bcb8adb9224d60df30cba904c2d2e5fe9b8
algoldbe <45243333+algoldbe@users.noreply.github.com>
2023-05-02 20:20:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import asyncio
 import edge_tts
 
@@ -19,13 +19,17 @@ def generate():
     text = f"Hello, you have reached {name} at Cisco Tac, my working hours are from 10 am to 6 pm Monday through Friday, so I'm not available right now, Please leave your name and service request number, I'll review the information and contact you back as soon as possible, thank you!"
     output_file = "vm.wav"
     asyncio.run(_generate_voice(text, voice, output_file))
-    message = "Your new voicemail audio file has been generated successfully, please download it from the link below and upload it to your phone profile."
-    file_url = f"/static/{output_file}"
+    message = "Your new voicemail audio file has been generated successfully."
+    file_url = f"/download/{output_file}"
     return render_template('result.html', message=message, file_url=file_url)
+
+@app.route('/download/<path:filename>', methods=['GET'])
+def download(filename):
+    return send_file(filename, as_attachment=True)
 
 async def _generate_voice(text, voice, output_file):
     communicate = edge_tts.Communicate(text, voice)
     await communicate.save(output_file)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host='0.0.0.0', port=80, debug=False)
