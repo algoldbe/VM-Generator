8a4fd8d1c6d4660a2515badc9e616b97618a98c3
algoldbe <45243333+algoldbe@users.noreply.github.com>
2023-06-02 19:25:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, send_file
 import asyncio
 import edge_tts
+from pydub import AudioSegment
+import os
 
 app = Flask(__name__)
 
@@ -17,10 +19,18 @@ def generate():
     else:
         voice = 'en-US-JennyNeural'
     text = f"Hello, you have reached {name} at Cisco Tac, my working hours are from 9 am to 5 pm Monday through Friday, Pacific Time, so I'm not available right now, Please leave your name and service request number, I'll review the information and contact you as soon as possible. For immediate Tac assistance, please call 001,800,553,2447; Thank you!"
-    output_file = "vm.wav"
+    output_file = "vm.mp3"
     asyncio.run(_generate_voice(text, voice, output_file))
+
+    # Convert the output file to WAV format
+    wav_output_file = "vm.wav"
+    convert_to_wav(output_file, wav_output_file)
+
+    # Delete the original MP3 file
+    os.remove(output_file)
+
     message = "Your new voicemail has been generated successfully!"
-    file_url = f"/download/{output_file}"
+    file_url = f"/download/{wav_output_file}"
     return render_template('result.html', message=message, file_url=file_url)
 
 @app.route('/download/<path:filename>', methods=['GET'])
@@ -29,7 +39,11 @@ def download(filename):
 
 async def _generate_voice(text, voice, output_file):
     communicate = edge_tts.Communicate(text, voice)
-    await communicate.save(output_file, channels=1)
+    await communicate.save(output_file)
+
+def convert_to_wav(input_file, output_file):
+    audio = AudioSegment.from_mp3(input_file)
+    audio.export(output_file, format="wav")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=False)
