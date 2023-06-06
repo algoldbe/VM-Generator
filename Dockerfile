336d7d2c02893a381fe53d7af15db2747dcab7b9
Lerosale99 <lerosale@cisco.com>
2023-06-06 15:17:31 -0500

    Adding ubuntu, ffmpeg and pydub

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
-FROM python:3-alpine3.15
+FROM ubuntu
+ENV http_proxy http://proxy.esl.cisco.com
+ENV https_proxy http://proxy.esl.cisco.com 
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y upgrade && apt-get install -y ffmpeg && apt-get install -y  python3 && apt-get install -y python3-pip
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, send_file
 import asyncio
 import edge_tts
-
+from pydub import AudioSegment
+import os
 app = Flask(__name__)
-
+HTTP_PROXY = "http://proxy.esl.cisco.com:8080" 
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -17,19 +18,25 @@ def generate():
     else:
         voice = 'en-US-JennyNeural'
     text = f"Hello, you have reached {name} at Cisco Tac, my working hours are from 9 am to 5 pm Monday through Friday, Pacific Time, so I'm not available right now, Please leave your name and service request number, I'll review the information and contact you as soon as possible. For immediate Tac assistance, please call 001,800,553,2447; Thank you!"
-    output_file = "vm.wav"
-    asyncio.run(_generate_voice(text, voice, output_file))
+    output_file = "vm.mp3"
+    asyncio.run(_generate_voice(text, voice, output_file, HTTP_PROXY))
+    wav_output_file = "vm.wav" 
+    convert_to_wav(output_file, wav_output_file)
+    os.remove(output_file)
     message = "Your new voicemail has been generated successfully!"
-    file_url = f"/download/{output_file}"
+    file_url = f"/download/{wav_output_file}"
     return render_template('result.html', message=message, file_url=file_url)
 
 @app.route('/download/<path:filename>', methods=['GET'])
 def download(filename):
     return send_file(filename, as_attachment=True)
 
-async def _generate_voice(text, voice, output_file):
-    communicate = edge_tts.Communicate(text, voice)
+async def _generate_voice(text, voice, output_file, proxy):
+    communicate = edge_tts.Communicate(text, voice, proxy=proxy)
     await communicate.save(output_file)
+def convert_to_wav(input_file, output_file):
+    audio = AudioSegment.from_mp3(input_file)
+    audio.export(output_file, format="wav")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=False)
