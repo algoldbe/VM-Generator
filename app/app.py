3d8a61be860c37f431d86d05d401c7f327abe4fd
Lerosale99 <lerosale@cisco.com>
2023-06-16 18:16:27 -0600

    Styles upgraded

--- app/app.py
+++ app/app.py
@@ -20,6 +20,7 @@ def generate():
     text = f"Hello, you have reached {name} at Cisco Tac, my working hours are from 9 am to 5 pm Monday through Friday, Pacific Time, so I'm not available right now, Please leave your name and service request number, I'll review the information and contact you as soon as possible. For immediate Tac assistance, please call 001,800,553,2447; Thank you!"
     output_file = "vm.mp3"
     asyncio.run(_generate_voice(text, voice, output_file, HTTP_PROXY))
+    asyncio.run(_generate_voice(text, voice, output_file))
     wav_output_file = "vm.wav" 
     convert_to_wav(output_file, wav_output_file)
     os.remove(output_file)
@@ -31,8 +32,8 @@ def generate():
 def download(filename):
     return send_file(filename, as_attachment=True)
 
-async def _generate_voice(text, voice, output_file, proxy):
-    communicate = edge_tts.Communicate(text, voice, proxy=proxy)
+async def _generate_voice(text, voice, output_file, http_proxy):
+    communicate = edge_tts.Communicate(text, voice, proxy=http_proxy)
     await communicate.save(output_file)
 def convert_to_wav(input_file, output_file):
     audio = AudioSegment.from_mp3(input_file)
